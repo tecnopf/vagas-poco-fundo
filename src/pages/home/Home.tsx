c9854335e603b79d01d3019c87ab51b509b5e932
tecnopf <tecnologiapf2124@gmail.com>
2025-09-09 13:25:05 -0300

    init

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <section className="hero">
+      <h1>Vagas de Emprego - Poço Fundo</h1>
+      <p>Encontre as melhores oportunidades de trabalho na nossa cidade.</p>
+      <a href="#vagas" className="btn">Ver Vagas</a>
+    </section>
+  );
+}
+
+export default Home;
